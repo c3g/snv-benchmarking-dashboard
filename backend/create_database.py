6824b83582743ce47fcb1bb720f4cbd90a406c99
SoGa83 <soofiagarmeh@gmail.com>
2025-08-03 20:53:40 -0400

    Add logging to create_database

--- backend/create_database.py
+++ backend/create_database.py
@@ -2,26 +2,25 @@ from database import create_tables, test_connection
 from models import *
 from populate_metadata import *
 import sys
+import logging 
+
+#logging
+logger = logging.getLogger(__name__)
 
 def main():
     """Main function to create database and populate reference data"""
-    print("Creating Benchmarking Database\n")
+    logging.info("Creating Benchmarking Database")
     
     # Test connection
     if not test_connection():
-        print("Cannot connect to database.")
+        logging.warning("Cannot connect to database")
         sys.exit(1)
     
     # Create tables
     create_tables() 
     
     # Add reference data
-    if populate_database_from_csv():
-        print("completed")
-
-    else:
-        print("Database setup failed")
-        sys.exit(1)
+    populate_database_from_csv()
 
 if __name__ == "__main__":
     main()
